ab62df404bceb57340fced64892ba540f952e071
ZorAnderius <gabriel.zorander@gmail.com>
2024-11-20 14:48:01 +0000

    add logic for sorting files by their types

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # goit-pythonweb-hw-04
+
+Sort all files to default folder 
+
+```bash 
+ python main.py --source source_folder
+```
+
+
+Sort all files to custom folder 
+
+```bash
+ python main.py --source source_folder --output MY_CUSTOM_FOLDER
+```
--- main.py
+++ main.py
@@ -1,5 +1,38 @@
-def run():
-    pass
+import argparse
+import asyncio
+import logging
+
+from typing import Coroutine, Any
+from aioshutil import copyfile
+from aiopath import AsyncPath
+
+parser = argparse.ArgumentParser(description="Copy files from one directory to another and sort them.")
+parser.add_argument('--source', '-s', help='Source directory', required=True)
+parser.add_argument('--output', '-d', help='Destination directory', default='output_folder')
+
+args = vars(parser.parse_args())
+
+source = AsyncPath(args.get('source'))
+output = AsyncPath(args.get('output'))
+
+async def read_folder(path: AsyncPath) -> Coroutine[Any, Any, None]:
+    try:
+        async for file in path.iterdir():
+            if await file.is_dir():
+                await  read_folder(file)
+            else:
+                await copy_file(file)
+    except FileNotFoundError as e:
+        logging.error(e)
+
+async def copy_file(file: AsyncPath) -> Coroutine[Any, Any, None]:
+    new_folder = output / file.suffix[1:]
+    try:
+        await new_folder.mkdir(exist_ok=True, parents=True)
+        await copyfile(file, new_folder / file.name)
+    except OSError as e:
+        logging.error(e)
 
 if __name__ == "__main__":
-    run()
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    asyncio.run(read_folder(source))
